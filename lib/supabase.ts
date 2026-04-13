df62d2ff5373c0e4e188cff8f6c18f9216561775
Jacob Chen <jacobchen0529@gmail.com>
2026-04-12 21:10:10 -0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -27,3 +27,57 @@ export async function fetchShoes() {
 
   return data;
 }
+
+export async function login(email: string, password: string) {
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      console.error('Error logging in:', error);
+      return null;
+    }
+
+    return data.session;
+  } catch (error) {
+    console.error('Error logging in:', error);
+    return null;
+  }
+}
+
+export async function register(email: string, password: string, firstName: string, lastName: string) {
+  try {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      console.error('Error registering:', error);
+      return null;
+    }
+
+    const { data: userData } = await supabase.auth.getUser();
+    // Store additional user information in the user_profile table
+    const { error: insertError } = await supabase.from('user_profile').insert({
+      id: userData.user?.id,
+      email,
+      first_name: firstName,
+      last_name: lastName,
+    });
+
+    console.log('Inserting additional user information...');
+
+    if (insertError) {
+      console.error('Error inserting additional user information:', insertError);
+      return null;
+    }
+
+    return 'User registered successfully!';
+  } catch (error) {
+    console.error('Error registering:', error);
+    return null;
+  }
+}
