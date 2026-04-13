48a2096471a9e0ff386034c5f8dcb667a90d947e
Jacob Chen <jacobchen0529@gmail.com>
2026-04-12 22:51:14 -0700

    Update questionnaire.tsx

--- pages/questionnaire.tsx
+++ pages/questionnaire.tsx
@@ -1,1 +1,18 @@
+import Navbar from '../components/Navbar';
 
+export default function Questionnaire() {
+  return (
+    <div>
+      <Navbar />
+
+      <section className="h-[80vh] flex flex-col justify-center items-center text-center bg-gray-100">
+        <h1 className="text-4xl font-bold mb-4">
+          Questionnaire
+        </h1>
+        <p className="text-gray-600">
+          This is where your questionnaire will go.
+        </p>
+      </section>
+    </div>
+  );
+}
